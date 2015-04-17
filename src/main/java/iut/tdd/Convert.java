204a25c10fbccb696328d024cc513d98127da97a
Guillaume Kleinpoort <guillaume.kleinpoort@gmail.com>
2015-04-17 14:20:16 +0200

    message

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -2,8 +2,41 @@ package iut.tdd;
 
 public class Convert {
 	public static String num2text(String input) {
-		return null;
+		switch (Integer.parseInt(input)) {
+		case 0:
+			return "zéro";
+		case 1:
+			return "un";
+
+		case 2:
+			return "deux";
+
+		case 3:
+			return "trois";
+
+		case 4:
+			return "quatre";
+
+		case 5:
+			return "cinq";
+
+		case 6:
+			return "six";
+
+		case 7:
+			return "sept";
+
+		case 8:
+			return "huit";
+
+		case 9:
+			return "neuf";
+
+		default:
+			return null;
+		}
 	}
+
 	public static String text2num(String input) {
 		return null;
 	}
